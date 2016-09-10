b3397bd825292ca6ee0e7aed54161283e31fa5fb
juliaryabinina <rowanjulerry@gmail.com>
2016-09-10 22:45:38 +0300

    rework

--- src/com/homework/FileSystem.java
+++ src/com/homework/FileSystem.java
@@ -1,8 +1,23 @@
 package com.homework;
 
+import com.homework.impl.Directory;
+
+import java.io.OutputStream;
+
 public interface FileSystem {
 
-    boolean mk(FileSystemElement fileSystemElement);
-    boolean rm(FileSystemElement fileSystemElement);
+    boolean newFile(String name);
+    boolean newDirectory(String name);
+    boolean copyDirectory(String name, Directory toDirectory);
+    boolean copyFile(String name, Directory toDirectory);
+    boolean deleteFile(String name);
+    boolean deleteDirectory(String name);
+    boolean renameFile(String newName, String oldName);
+    boolean renameDirectory(String newName, String oldName);
+    void listDirectoryContent();
+    OutputStream readFile(String name);
+    void editFile(String name);
+    boolean setCurrentDirectory(Directory directory);
+    Directory getCurrentDirectory(Directory directory);
 
 }
--- src/com/homework/FileSystemElement.java
+++ src/com/homework/FileSystemElement.java
@@ -2,9 +2,13 @@ package com.homework;
 
 import com.homework.impl.Directory;
 
+import java.time.LocalDateTime;
+
 public interface FileSystemElement {
 
     int getSize();
     Directory getParent();
+    LocalDateTime getCreationTime();
+    LocalDateTime getModificationTime();
 
 }
